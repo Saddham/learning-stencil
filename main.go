03b537880c28f2513b87a479671b45d7a849dfe7
saddham-pathan <spathan@reputation.com>
2023-11-09 21:08:54 +0530

    Using stencil native extension

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	fmt.Println("Hello, world!")
 
-    // <<Stencil::Block(additionalMessage)>>
+	// <<Stencil::Block(additionalMessage)>>
 
 	// <</Stencil::Block>>
 }
